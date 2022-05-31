0b7ca698ff48d3442f1993c122278c40455774e4
Daniel Gore <daniel.gore@netmatters-scs.com>
2022-05-31 10:05:00 +0100

    Moved the form into JS added animation to nav
    The form only gets displayed if JavaScript is Enabled and the navigation menu is now fixed at smaller screen sizes. The navigation menu is animated when JavaScript is enabled.

--- js/main.js
+++ js/main.js
@@ -7,6 +7,39 @@ const typed = new Typed(".auto-type", {
     smartBack: true
 })
 
+$("#contact").append(`
+    <div class="col-8">
+    <form id="contact-form" action="" method="post">
+        <div class="row">
+            <div class="col col-12 col-md-6">
+                <label class="required" for="first-name"></label>
+                <input class="form-control" type="text" id="first-name" placeholder="First Name*" required>
+            </div>
+            <div class="col col-12 col-md-6">
+                <label class="required" for="last-name"></label>
+                <input class="form-control" type="text" id="last-name" placeholder="Last Name*" required>
+            </div>
+            <div class="col col-12">
+                <label class="required" for="email"></label>
+                <input class="form-control" type="email" id="email" placeholder="Email Address*" required>
+            </div>
+            <div class="col col-12">
+                <label class="required" for="subject"></label>
+                <input class="form-control" type="text" id="subject" placeholder="Subject">
+            </div>
+            <div class="col col-12">
+                <label class="required" for="message"></label>
+                <textarea class="form-control textarea" id="message" placeholder="Message"></textarea>
+            </div>
+            <div class="col col-12">
+                <button class="btn btn-form" type="button" onclick="validateForm()">Submit</button>
+            </div>
+        </div>
+    </form>
+</div>
+`);
+$(".jSRequired").text("following contact form, ");
+
 //Add error border and error message
 function setError (i, errorMessage) {
     $($form[i]).css({"border": "solid 2px red"});
@@ -46,3 +79,14 @@ function validateForm(event) {
         }
     }
 }
+
+//Animate nav menu to slide down
+//If hamburger is pressed
+$(".hamburger").on("click", () => {
+    if ($(".toggler")[0].checked === false) {
+        $(".nav-fixed").slideDown("slow");
+    }
+    else {
+        $(".nav-fixed").slideUp("slow");
+    }
+});
